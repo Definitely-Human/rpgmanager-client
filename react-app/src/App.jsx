bc34b4fe93edd2d1641e51ae7892e368f30c50ca
DefinitelyHuman <odest66@gmail.com>
2023-05-12 19:08:34 +0200

    Css initial setup

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Error, Home, Play, Profile } from "./pages";

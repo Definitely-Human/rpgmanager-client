bbcb8144e1af8af34f423154390f06dec0c45dbe
DefinitelyHuman <odest66@gmail.com>
2023-05-12 15:34:38 +0200

    Added axios and react-router to deps. Initial router setup.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,38 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Error, Home, Play, Profile } from "./pages";
 
 function App() {
     const [count, setCount] = useState(0);
-
+    const token = "00d96b8d7c2a19530b776efe546c55c11eb4cbde";
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/manager/tasks", {
+                headers: { Authorization: `Token ${token}` },
+            })
+            .then((res) => {
+                console.log(res);
+            });
+    }, []);
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="play" element={<Play />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="*" element={<Error />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ react-app/src/pages/Play.jsx
@@ -0,0 +1,4 @@
+const Play = () => {
+    return <div>Play</div>;
+};
+export default Play;

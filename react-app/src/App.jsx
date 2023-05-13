376ee7ac0c62f46d4d3a6c04ed45a8023a8ce7d2
DefinitelyHuman <odest66@gmail.com>
2023-05-13 02:53:22 +0200

    Added GitHub actions.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Error, Home, Play, Profile } from "./pages";
 

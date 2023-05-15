f5f5455b98309e95decd6bc048e7fdbb0d8897eb
DefinitelyHuman <odest66@gmail.com>
2023-05-15 03:15:37 +0200

    Added redux toolkit.

--- react-app/src/components/TopBar.jsx
+++ react-app/src/components/TopBar.jsx
@@ -3,13 +3,13 @@ import UserToolbar from "./UserToolbar";
 
 const TopBar = () => {
     return (
-        <div className="flex justify-around items-center bg-gray-blue-700">
+        <header className="flex justify-around items-center bg-gray-blue-700">
             <h1 className="text-4xl text-slate-100 mb-2">
                 <span className="text-primary">RPG</span> Manager
             </h1>
             <GlobalSearch />
             <UserToolbar />
-        </div>
+        </header>
     );
 };
 export default TopBar;
--- react-app/src/pages/Play.jsx
+++ react-app/src/pages/Play.jsx
@@ -6,13 +6,13 @@ const Play = () => {
         <div className="grid grid-rows-[60px_minmax(300px,_1fr)] h-screen dark:text-slate-100">
             <TopBar />
 
-            <div className="grid grid-cols-[1fr_3fr_1fr]">
+            <main className="grid grid-cols-[1fr_3fr_1fr]">
                 <div className="bg-gray-blue-800 border-r-2 border-gray-900">
                     <CategoriesWindow />
                 </div>
                 <div className="bg-gray-blue-900"></div>
                 <div className="bg-gray-blue-800 border-l-2 border-gray-900"></div>
-            </div>
+            </main>
         </div>
     );
 };
